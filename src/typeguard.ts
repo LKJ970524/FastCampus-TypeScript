a017b6514f6d66495c9f1a7de30e0f846d6f03bc
LKJ970524 <dlrbwjd7096@naver.com>
2023-11-06 22:33:11 +0900

    typeGuard 학습

---
+++ src/typeguard.ts
@@ -0,0 +1,24 @@
+// 타입 가드(Guards)
+
+// function logText(el : Element) {
+//   console.log(el.textContent);
+// }
+
+// const h1El = document.querySelector('h1')
+// if (h1El instanceof HTMLHeadingElement) {
+//   logText(h1El)
+// }
+
+function add(val: string | number | boolean) {
+  let res = 'Result => '
+  if (typeof val === 'number') {
+    res += val.toFixed(2)
+  }
+  if (typeof val === 'string') {
+    res += val.toUpperCase()
+  }
+  console.log(res);  
+}
+
+add(3.1415926535)
+add('hello world')
